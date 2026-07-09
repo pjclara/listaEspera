f606678292f5c48229b9a7fc546f90e13020ac85
Paulo Clara <59574041+pjclara@users.noreply.github.com>
2026-07-09 09:18:41 +0100

    Add new components for user authentication and profile management
    - Implement password update functionality with validation and error handling.
    - Create profile management interface allowing users to update their name and email.
    - Add registration and reset password forms with appropriate input validation.
    - Introduce email verification and welcome components for user onboarding.
    - Enhance UI with loading indicators and transition effects for better user experience.

---
+++ public/build/assets/input-error-D9yYfFUv.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CEIBkgSC.js";import{c as o}from"./button-CUZ4iQt2.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
